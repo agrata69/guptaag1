595d9e2a9fd614dc33a023b127f2a4cdca8713b8
Agrata Gupta <agrata.gupta@mail.utoronto.ca>
2024-10-13 19:33:54 -0400

    Task 1.1 third

--- week4git/src/main/java/edu/csc207/fall2024/Play.java
+++ week4git/src/main/java/edu/csc207/fall2024/Play.java
@@ -1,12 +1,31 @@
 package edu.csc207.fall2024;
 
-public class Play {
+/**
+ * * @null This class does not accept null values for name.
+ */
+public final class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 }
