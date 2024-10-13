df1dcacfe4f22773cf78966c029bd20de18f4264
Agrata Gupta <agrata.gupta@mail.utoronto.ca>
2024-10-13 19:33:32 -0400

    Task 1.1 second

--- week4git/src/main/java/edu/csc207/fall2024/Performance.java
+++ week4git/src/main/java/edu/csc207/fall2024/Performance.java
@@ -1,12 +1,32 @@
 package edu.csc207.fall2024;
 
-public class Performance {
+/**
+ * * @null This class does not accept null values for playID.
+ **/
 
-    public String playID;
-    public int audience;
+public final class Performance {
+
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
+        this.audience = audience;
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public void setPlayID(String playID) {
+        this.playID = playID;
+    }
+
+    public int getAudience() {
+        return audience;
+    }
+
+    public void setAudience(int audience) {
         this.audience = audience;
     }
 }
