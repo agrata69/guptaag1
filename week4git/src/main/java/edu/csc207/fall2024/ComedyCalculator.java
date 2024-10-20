75d7ffedc7a508101fe87256a3e4d43597870ce5
Agrata Gupta <agrata.gupta@mail.utoronto.ca>
2024-10-19 23:02:39 -0400

    Task 4.3 even more finish

--- week4git/src/main/java/edu/csc207/fall2024/ComedyCalculator.java
+++ week4git/src/main/java/edu/csc207/fall2024/ComedyCalculator.java
@@ -1,4 +1,25 @@
 package edu.csc207.fall2024;
 
-public class ComedyCalculator {
+public class ComedyCalculator extends AbstractPerformanceCalculator {
+
+    public ComedyCalculator(Performance performance, Play play) {
+        super(performance, play);
+    }
+
+    @Override
+    public int getAmount() {
+        int result = Constants.COMEDY_BASE_AMOUNT;
+        if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+            result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+        }
+        result += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
+        return result;
+    }
+
+    @Override
+    public int getVolumeCredits() {
+        int result;
+        result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        return result;
+    }
 }
