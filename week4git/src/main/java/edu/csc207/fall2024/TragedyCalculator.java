4bf5b1536f10490807077ed79e678495c8713f6b
Agrata Gupta <agrata.gupta@mail.utoronto.ca>
2024-10-19 23:02:19 -0400

    Task 4.3 more finish

--- week4git/src/main/java/edu/csc207/fall2024/TragedyCalculator.java
+++ week4git/src/main/java/edu/csc207/fall2024/TragedyCalculator.java
@@ -1,4 +1,22 @@
 package edu.csc207.fall2024;
 
-public class TragedyCalculator {
+public class TragedyCalculator extends AbstractPerformanceCalculator {
+
+    public TragedyCalculator(Performance performance, Play play) {
+        super(performance, play);
+    }
+
+    @Override
+    public int getAmount() {
+        int result = StatementPrinter.TRAGEDY_BASE_AMOUNT;
+        if (performance.getAudience() > StatementPrinter.TRAGEDY_AUDIENCE_THRESHOLD) {
+            result += StatementPrinter.TRAGEDY_EXTRA_AUDIENCE_AMOUNT * (performance.getAudience() - StatementPrinter.TRAGEDY_AUDIENCE_THRESHOLD);
+        }
+        return result;
+    }
+
+    @Override
+    public int getVolumeCredits() {
+        return 0;
+    }
 }
